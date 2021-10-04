1c8f0937e5931a9641e4fb53662ad2b018f0c4f0
ManOfCheese <samwalet16@gmail.com>
2021-10-04 16:13:29 +0200

    First Commit

---
+++ JackOfController/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraManager : MonoBehaviour {
+
+	[Header( "Camera Settings" )]
+	[Tooltip("When true camera controls will be inverted meaning moving left will move the camera to the right.")]
+	public bool inverted;
+	[Tooltip( "Determines how much the camera moves relative to the input." )]
+	[Range( 0.0f, 1.0f)]
+	public float senstivity;
+	[Tooltip( "How many degrees the camera can rotate upwards before locking in place." )]
+	public float xRotationLimitsUp = -90f;
+	[Tooltip( "How many degrees the camera can rotate downwards before locking in place" )]
+	public float xRotationLimitsDown = 60f;
+
+	[Header( "Crouch Settings" )]
+	[Tooltip( "The height of the camera when crouching" )]
+	public float crouchCamHeight = 0.5f;
+
+	[Header( "Head Bob Settings" )]
+	[Tooltip( "When true the camera moves up and down to simulate the movement of someone walking." )]
+	public bool headBob;
+	[Tooltip( "How quickly the camera moves up and down." )]
+	public float headBobSpeed;
+	[Tooltip( "How far up and down the camera moves, if  this is higher it the camera will also need to move faster to cover the distance." )]
+	public float headBobIntensity;
+
+	[Header( "FOV Boost Settings" )]
+	[Tooltip( "How long does the FOV boost last." )]
+	public float FOVBoostDuration;
+	[Tooltip( "How much does the FOV increase." )]
+	public float FOVBoostIntensity;
+	[Tooltip( "Curve used to increase and decrease FOV when FOV boost happens." )]
+	public AnimationCurve FOVBoostCurve;
+
+	[Header( "Screen Tilt Settings" )]
+	[Tooltip( "How many degrees does the screen tilt." )]
+	public float maximumTilt;
+
+	private Camera cam;
+	private float camHeight;
+	private float camStartHeight;
+	private float xCamRotation = 0.0f;
+	private float yCamRotation = 0.0f;
+	public Vector2 lookVector;
+
+	private void Awake() {
+		cam = GetComponent<Camera>();
+		camStartHeight = cam.transform.localPosition.y;
+		camHeight = camStartHeight;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	public void OnLook( InputAction.CallbackContext value ) {
+		Vector2 mouseLook = value.ReadValue<Vector2>();
+		lookVector = new Vector2( mouseLook.y, mouseLook.x );
+	}
+
+	public void OnCrouch( InputAction.CallbackContext value ) {
+		Debug.Log( "crouch" );
+		camHeight = crouchCamHeight;
+	}
+
+	private void Update() {
+		xCamRotation -= senstivity * lookVector.x;
+		yCamRotation += senstivity * lookVector.y;
+
+		xCamRotation %= 360;
+		yCamRotation %= 360;
+		xCamRotation = Mathf.Clamp( xCamRotation, xRotationLimitsUp, xRotationLimitsDown );
+		transform.eulerAngles = new Vector3( xCamRotation, yCamRotation, 0f );
+	}
+}
---
+++ JackOfController/Assets/Scripts/CheeseController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum AerialMovementSettings {
+    FullMovement,
+    FullCameraMovement,
+    LimitedCameraMovement,
+    NoMovement
+}
+
+public class CheeseController : MonoBehaviour {
+
+    public CharacterController cc;
+    public Camera cam;
+
+    [Header( "Movement Settings" )]
+    [Tooltip("Speed of movement in units per second")]
+    public float speed = 5f;
+    public float stickToGroundForce = 1f;
+    public AnimationCurve startSprintCurve;
+    public AnimationCurve endSprintCurve;
+
+    [Header( "Jump Settings" )]
+    [Tooltip( "The upward force applied when the player jumps" )]
+    public float jumpForce = 5f;
+    [Tooltip( "How long it takes to complete and uninterrupted jump" )]
+    public float jumpDuration = 1f;
+    [Tooltip( "How the jump force is applied" )]
+    public AnimationCurve jumpArc;
+    [Tooltip( "How many times the player can jump" )]
+    public int jumps = 1;
+
+    [Header( "Sprint Settings" )]
+    [Tooltip( "Is sprinting enabled" )]
+    public bool sprintAllowed = true;
+    [Tooltip( "Should the FOV momentarily increase when you start sprinting" )]
+    public bool FOVBurst = true;
+    [Tooltip( "Should the FOV increase when you start sprinting" )]
+    public bool FOVBoost = true;
+    [Tooltip( "Sprinting speed in units per second" )]
+    public float sprintSpeed = 10f;
+    [Tooltip( "Sprinting speed relative to the walking speed" )]
+    public float relativeSprintSpeed = 2f;
+
+    [Header( "Aerial Movement Settings" )]
+    [Tooltip( "How much freedom of movement should the player have in mid-air?" )]
+    public AerialMovementSettings aerialMovement;
+    [Tooltip( "How fast can the player change direction in mid-air with limited camera movement" )]
+    public float aerialTurnSpeed;
+
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private bool sprinting = false;
+    private bool jump = false;
+    private bool grounded = true;
+    private float jumpTimeStamp;
+    private Vector2 rawMovementVector;
+
+    private void Awake() {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+        playerInput.ActivateInput();
+        playerInput.SwitchCurrentActionMap( "PlayerControls" );
+	}
+
+	public void OnMove( InputAction.CallbackContext value ) {
+        rawMovementVector = value.ReadValue<Vector2>();
+    }
+
+    public void OnSprint( InputAction.CallbackContext value ) {
+        if ( value.started ) sprinting = true;
+        if ( value.canceled ) sprinting = false;
+    }
+
+    public void OnJump( InputAction.CallbackContext value ) {
+        if ( value.performed ) {
+            jump = true;
+		}
+    }
+
+	private void Update() {
+        float moveSpeed = speed;
+
+        if ( sprintAllowed ) {
+            if ( sprinting ) {
+                if ( relativeSprintSpeed == 0f ) {
+                    moveSpeed = sprintSpeed;
+                }
+                else {
+                    moveSpeed = speed * relativeSprintSpeed;
+                }
+            }
+        }
+
+        if ( jump ) {
+            jumpTimeStamp = Time.time;
+            jump = false;
+        }
+
+        Vector3 forwardVector = ( transform.position + cam.transform.forward - transform.position ).normalized;
+        Vector3 rightVector = ( transform.position + cam.transform.right - transform.position ).normalized;
+        Vector3 relativeMovementVector = rawMovementVector.x * rightVector + rawMovementVector.y * forwardVector;
+        Vector3 finalMovementVector = new Vector3( relativeMovementVector.x * moveSpeed, -stickToGroundForce, relativeMovementVector.z * moveSpeed );
+
+        if ( jumpTimeStamp != 0f ) {
+            float timePassed = Time.time - jumpTimeStamp;
+            if ( timePassed <= jumpDuration ) {
+                float adjustedJumpForce = jumpForce * jumpArc.Evaluate( timePassed / jumpDuration );
+                finalMovementVector += new Vector3( 0f, stickToGroundForce + adjustedJumpForce, 0f );
+            }
+            else {
+                jumpTimeStamp = 0f;
+            }
+        }
+
+        cc.Move( finalMovementVector * Time.deltaTime );
+	}
+}
